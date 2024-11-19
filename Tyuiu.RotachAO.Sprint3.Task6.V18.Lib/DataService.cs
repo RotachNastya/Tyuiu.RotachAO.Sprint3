eafef60c653916e5771f22376e7fb960f437906e
RotachNastya <education.for.nastya@gmail.com>
2024-11-19 21:09:27 +0500

    Task6

--- Tyuiu.RotachAO.Sprint3.Task6.V18.Lib/DataService.cs
+++ Tyuiu.RotachAO.Sprint3.Task6.V18.Lib/DataService.cs
@@ -5,23 +5,21 @@ namespace Tyuiu.RotachAO.Sprint3.Task6.V18.Lib;
 public class DataService : ISprint3Task6V18
 {
     public int GetSumTheDivisors(int startValue, int stopValue)
-    {
-            
+     {  
                 int sum = 0;
                 int dsum;
-        for (int i = startValue; i <= stopValue; i++)
-
-        {
-            dsum = 0;
-            for (int d = i; d <= i; d++)
+                  for (int i = startValue; i <= stopValue; i++)
+                {
+               dsum = 0;
+               for (int d = i; d <= i; d++)
                 if (i % d == 0)
                     dsum += d;
-            if (dsum > 10)
+                if (dsum > 10)
                 sum += dsum;
-        }
+               }
                     return sum;
-                }
     }
 }
+
 
 
