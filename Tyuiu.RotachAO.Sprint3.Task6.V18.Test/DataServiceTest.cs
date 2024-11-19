aba4cf94d24c3145d8d4d8d4c7eefa0ea88964e8
RotachNastya <education.for.nastya@gmail.com>
2024-11-19 21:05:04 +0500

    Task6

--- Tyuiu.RotachAO.Sprint3.Task6.V18.Test/DataServiceTest.cs
+++ Tyuiu.RotachAO.Sprint3.Task6.V18.Test/DataServiceTest.cs
@@ -3,7 +3,7 @@ using Tyuiu.RotachAO.Sprint3.Task6.V18.Lib;
 namespace Tyuiu.RotachAO.Sprint3.Task6.V18.Test; 
  
 [TestClass] 
-public class DataServiceTest 
+public class DataServiceTest  
 { 
    [TestMethod] 
    public void ValidExpression()  
