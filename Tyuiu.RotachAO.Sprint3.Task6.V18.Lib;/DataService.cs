95c5869f8ad59a0443c8904fc159de907c7fae23
RotachNastya <education.for.nastya@gmail.com>
2024-11-19 22:00:04 +0500

    Task6

---
+++ Tyuiu.RotachAO.Sprint3.Task6.V18.Lib;/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.RotachAO.Sprint3.Task6.V18.Lib_
+{
+    public class Class1
+    {
+
+    }
+}
--- Tyuiu.RotachAO.Sprint3.Task6.V18.Test/DataServiceTest.cs
+++ Tyuiu.RotachAO.Sprint3.Task6.V18.Test/DataServiceTest.cs
@@ -3,18 +3,10 @@ using Tyuiu.RotachAO.Sprint3.Task6.V18.Lib;
 namespace Tyuiu.RotachAO.Sprint3.Task6.V18.Test; 
  
 [TestClass] 
-public class DataServiceTest  
+public class DataServiceTest 
 { 
    [TestMethod] 
-   public void ValidGetSumTheDivisors()  
-   {
-        DataService ds = new DataService();
-
-        int startValue = 9;
-        int stopValue = 18;
-
-        int res = ds.GetSumTheDivisors(startValue, stopValue);
-        int wait = 95;
-        Assert.AreEqual(res, wait);
+   public void ValidExpression()  
+   { 
    } 
 } 
